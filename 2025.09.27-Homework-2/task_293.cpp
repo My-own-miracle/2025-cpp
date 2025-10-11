d3e8d659030be7f9159c9cdcf86d819a7e59fd13
My-own-miracle <platon21ka@gmail.com>
2025-10-11 15:41:00 +0300

    2025.09.27-Homework-2

--- 2025.09.27-Homework-2/task_293.cpp
+++ 2025.09.27-Homework-2/task_293.cpp
@@ -1,43 +1,44 @@
 #include <cstdio>
-#include <string>
 #include <cmath>
 
 int main(int argc, char** argv)
 {
-    char start [4];
-    char end [4];
-    scanf("%s %s", &start, &end);
+    char stlet = ' ';
+    char endlet = ' ';
+    int stnum = 0;
+    int endnum = 0;
+    scanf("%c%d %c%d", &stlet, &stnum, &endlet, &endnum);
     bool flag = true;
-    if (abs(start[0] - end[0]) <= 1 and abs(start[1] - end[1]) <= 1)
+    if (fabs(stlet - endlet) <= 1 and fabs(stnum - endnum) <= 1)
     {
         flag = false;
         printf("King\n");
     }
-    if (abs(start[0] - end[0]) == abs(start[1] - end[1]) or
-            start[0] == end[0] or start[1] == end[1])
+    if (fabs(stlet - endlet) == fabs(stnum - endnum) or
+            stlet == endlet or stnum == endnum)
     {
         flag = false;
         printf("Queen\n");
     }
-    if (start[0] == end[0] or start[1] == end[1])
+    if (stlet == endlet or stnum == endnum)
     {
         flag = false;
         printf("Rook\n");
     }
-    if (abs(start[0] - end[0]) == abs(start[1] - end[1]))
+    if (fabs(stlet - endlet) == fabs(stnum - endnum))
     {
         flag = false;
         printf("Bishop\n");
     }
-    if (abs(start[0] - end[0]) + abs(start[1] - end[1]) == 3)
+    if (fabs(stlet - endlet) + fabs(stnum - endnum) == 3)
     {
         flag = false;
         printf("Knight\n");
     }
-    if (start[1] != char('1'))
+    if (stnum != 1)
     {
-        if (start[0] == end[0] and (end[1] - start[1] == 1 or 
-            (end[1] - start[1] == 2 and start[1] == char('2'))))
+        if (stlet == endlet and (endnum - stnum == 1 or 
+            (endnum - stnum == 2 and stnum == 2)))
         {
             flag = false;
             printf("Pawn\n");
