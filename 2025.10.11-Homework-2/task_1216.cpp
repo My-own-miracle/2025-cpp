8da654e15082908ff2b21629bcc24fe19847c534
My-own-miracle <platon21ka@gmail.com>
2025-10-18 16:54:08 +0300

    Update task_1216.cpp

--- 2025.10.11-Homework-2/task_1216.cpp
+++ 2025.10.11-Homework-2/task_1216.cpp
@@ -10,21 +10,21 @@ int main(int argc, char** argv)
     int c2 = 0;
     scanf("%d %d %d", &a1, &b1, &c1);
     scanf("%d %d %d", &a2, &b2, &c2);
-    int aa1 = (a1 < b1 and a1 < c1 ? a1 : (b1 < c1 ? b1 : c1));
-    int cc1 = (a1 > b1 and a1 > c1 ? a1 : (b1 > c1 ? b1 : c1));
+    int aa1 = (a1 < b1 && a1 < c1 ? a1 : (b1 < c1 ? b1 : c1));
+    int cc1 = (a1 > b1 && a1 > c1 ? a1 : (b1 > c1 ? b1 : c1));
     int bb1 = a1 + b1 + c1 - aa1 - cc1;
-    int aa2 = (a2 < b2 and a2 < c2 ? a2 : (b2 < c2 ? b2 : c2));
-    int cc2 = (a2 > b2 and a2 > c2 ? a2 : (b2 > c2 ? b2 : c2));
+    int aa2 = (a2 < b2 && a2 < c2 ? a2 : (b2 < c2 ? b2 : c2));
+    int cc2 = (a2 > b2 && a2 > c2 ? a2 : (b2 > c2 ? b2 : c2));
     int bb2 = a2 + b2 + c2 - aa2 - cc2;
-    if (aa1 == aa2 and bb1 == bb2 and cc1 == cc2)
+    if (aa1 == aa2 && bb1 == bb2 && cc1 == cc2)
     {
         printf("Boxes are equal");
     }
-    else if (aa1 <= aa2 and bb1 <= bb2 and cc1 <= cc2)
+    else if (aa1 <= aa2 && bb1 <= bb2 && cc1 <= cc2)
     {
         printf("The first box is smaller than the second one");
     }
-    else if (aa1 >= aa2 and bb1 >= bb2 and cc1 >= cc2)
+    else if (aa1 >= aa2 && bb1 >= bb2 && cc1 >= cc2)
     {
         printf("The first box is larger than the second one");
     }
