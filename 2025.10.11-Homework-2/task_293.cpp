140e0f67ff3571ffeed681a21777e7c571787acd
My-own-miracle <platon21ka@gmail.com>
2025-10-18 17:00:06 +0300

    Update task_293.cpp

--- 2025.10.11-Homework-2/task_293.cpp
+++ 2025.10.11-Homework-2/task_293.cpp
@@ -9,17 +9,17 @@ int main(int argc, char** argv)
     int endnum = 0;
     scanf("%c%d %c%d", &stlet, &stnum, &endlet, &endnum);
     bool flag = true;
-    if (fabs(stlet - endlet) <= 1 and fabs(stnum - endnum) <= 1)
+    if (fabs(stlet - endlet) <= 1 && fabs(stnum - endnum) <= 1)
     {
         flag = false;
         printf("King\n");
     }
-    if (fabs(stlet - endlet) == fabs(stnum - endnum) or stlet == endlet or stnum == endnum)
+    if (fabs(stlet - endlet) == fabs(stnum - endnum) || stlet == endlet || stnum == endnum)
     {
         flag = false;
         printf("Queen\n");
     }
-    if (stlet == endlet or stnum == endnum)
+    if (stlet == endlet || stnum == endnum)
     {
         flag = false;
         printf("Rook\n");
@@ -36,7 +36,7 @@ int main(int argc, char** argv)
     }
     if (stnum != 1)
     {
-        if (stlet == endlet and (endnum - stnum == 1 or (endnum - stnum == 2 and stnum == 2)))
+        if (stlet == endlet && (endnum - stnum == 1 || (endnum - stnum == 2 && stnum == 2)))
         {
             flag = false;
             printf("Pawn\n");
